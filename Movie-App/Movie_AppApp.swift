7858c1bd14f0a9e917cf82dad795f2bf949567a4
AdrienBonv <60156774+AdrienBonv@users.noreply.github.com>
2024-10-02 22:41:28 +0200

    Initial Commit

---
+++ Movie-App/Movie_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_AppApp.swift
+//  Movie-App
+//
+//  Created by Adrien BONVALLET on 02/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct Movie_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
